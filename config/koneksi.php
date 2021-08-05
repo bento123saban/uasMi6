687025f49148e502815a5ec0b3e50017516baa45
bento123saban <82221414+bento123saban@users.noreply.github.com>
2021-08-06 01:37:24 +0900

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,1 +1,17 @@
+<?php
+    require_once 'function.php';
 
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "putriAngkasa";
+
+    $con = mysqli_connect($host,$user,$pass,$db);
+
+    if(mysqli_connect_errno())
+    {
+        echo "Koneksi Gagal";
+        mysqli_connect_error();
+    }
+
+?>
