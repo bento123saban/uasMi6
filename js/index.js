0f7bbb8267568ffa14129f85cf803e4496fead8b
Bendhard16 <82221414+bento123saban@users.noreply.github.com>
2021-08-06 01:45:38 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 
+/* ----------- menu search ------------- */
+
+function search(el) {
+	const input = el.value.toUpperCase();
+	document.querySelectorAll('.nama-menu').forEach( function(menu) {
+		if (menu.textContent.toUpperCase().indexOf(input) > -1) {
+			menu.parentElement.style.display = ''
+		} else {
+			menu.parentElement.style.display = 'none'
+		}
+	})
+}
